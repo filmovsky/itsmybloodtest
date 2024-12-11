01fe17961341b0aebd4fc7a2b216063a5e2baa59
filmovsky <marsie26@wp.pl>
2024-12-11 19:31:43 +0100

    Update interpretation.js

--- pages/pages/api/interpretation.js
+++ pages/pages/api/interpretation.js
@@ -1,1 +1,8 @@
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    res.status(200).json({ message: 'POST request received!' });
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
 
