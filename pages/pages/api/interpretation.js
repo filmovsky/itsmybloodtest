5e447632c9e7ea2340e0699008195a9c2b931645
filmovsky <marsie26@wp.pl>
2024-12-11 20:06:34 +0100

    Delete pages/pages/api directory

--- pages/pages/api/interpretation.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    if (req.method === 'POST') {
-        res.status(200).json({ message: 'POST request received!' });
-    } else {
-        res.status(405).json({ message: 'Method not allowed' });
-    }
-}
